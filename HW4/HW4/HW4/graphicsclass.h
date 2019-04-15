9a3026b12ec2356118610b4e25ba020954f4cf93
ByeongChang <like98100@naver.com>
2019-04-15 17:41:56 +0900

    HW4
    complete

---
+++ HW4/HW4/HW4/graphicsclass.h
@@ -0,0 +1,58 @@
+#pragma once
+////////////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+//////////////
+// INCLUDES //
+//////////////
+#include <windows.h>
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "d3dclass.h"
+
+#include "cameraclass.h"
+#include "modelclass.h"
+#include "colorshaderclass.h"
+
+#include "textureshaderclass.h"
+/////////////
+// GLOBALS //
+/////////////
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+
+#include "inputclass.h"
+////////////////////////////////////////////////////////////////////////////////
+// Class name: GraphicsClass
+////////////////////////////////////////////////////////////////////////////////
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+	void SetSampling(int, HWND);
+private:
+	bool Render();
+private:
+
+	D3DClass* m_D3D;
+
+	CameraClass* m_Camera;
+	ModelClass* m_Model;
+	//ColorShaderClass* m_ColorShader;
+	
+	TextureShaderClass* m_TextureShader;
+	InputClass* m_Input;
+};
+#endif
---
+++ HW4/HW4/HW4/modelclass.cpp
@@ -0,0 +1,249 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: modelclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "modelclass.h"
+
+ModelClass::ModelClass()
+{
+	m_vertexBuffer = 0;
+	m_indexBuffer = 0;
+
+	m_Texture = 0;
+}
+ModelClass::ModelClass(const ModelClass& other)
+{
+}
+ModelClass::~ModelClass()
+{
+}
+
+bool ModelClass::Initialize(ID3D11Device* device, WCHAR* textureFilename)
+{
+	bool result;
+	// Initialize the vertex and index buffer that hold the geometry for the triangle.
+	result = InitializeBuffers(device);
+	if (!result)
+	{
+		return false;
+	}
+
+	// Load the texture for this model.
+	result = LoadTexture(device, textureFilename);
+	if (!result)
+	{
+		return false;
+	}
+	return true;
+}
+
+void ModelClass::Shutdown()
+{
+	// Release the model texture.
+	ReleaseTexture();
+
+	// Release the vertex and index buffers.
+	ShutdownBuffers();
+	return;
+}
+
+void ModelClass::Render(ID3D11DeviceContext* deviceContext)
+{
+	// Put the vertex and index buffers on the graphics pipeline to prepare them for drawing.
+	RenderBuffers(deviceContext);
+	return;
+}
+
+int ModelClass::GetIndexCount()
+{
+	return m_indexCount;
+}
+
+ID3D11ShaderResourceView* ModelClass::GetTexture()
+{
+	return m_Texture->GetTexture();
+}
+
+bool ModelClass::InitializeBuffers(ID3D11Device* device)
+{
+	VertexType* vertices;
+	unsigned long* indices;
+	D3D11_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
+	D3D11_SUBRESOURCE_DATA vertexData, indexData;
+	HRESULT result;
+
+	// Set the number of vertices in the vertex array.
+	/*m_vertexCount = 18;*/
+	m_vertexCount = 12;
+	// Set the number of indices in the index array.
+	/*m_indexCount = 18;*/
+	m_indexCount = 12;
+	// Create the vertex array.
+	vertices = new VertexType[m_vertexCount];
+	if (!vertices)
+	{
+		return false;
+	}
+	// Create the index array.
+	indices = new unsigned long[m_indexCount];
+	if (!indices)
+	{
+		return false;
+	}
+
+	// Load the vertex array with data.
+
+
+	//4
+	vertices[0].position = D3DXVECTOR3(1.0f, -2.0f, 0.0f); 
+	vertices[0].texture = D3DXVECTOR2(0.0f, 1.0f);
+	vertices[1].position = D3DXVECTOR3(1.0f, 2.0f, 0.0f); 
+	vertices[1].texture = D3DXVECTOR2(0.0f, 0.0f);
+	vertices[2].position = D3DXVECTOR3(5.0f, 2.0f, 0.0f); 
+	vertices[2].texture = D3DXVECTOR2(1.0f, 0.0f);
+
+	vertices[3].position = D3DXVECTOR3(1.0f, -2.0f, 0.0f); 
+	vertices[3].texture = D3DXVECTOR2(0.0f, 1.0f);
+	vertices[4].position = D3DXVECTOR3(5.0f, 2.0f, 0.0f); 
+	vertices[4].texture = D3DXVECTOR2(1.0f, 0.0f);
+	vertices[5].position = D3DXVECTOR3(5.0f, -2.0f, 0.0f); 
+	vertices[5].texture = D3DXVECTOR2(1.0f, 1.0f);
+
+	vertices[6].position = D3DXVECTOR3(-5.0f, -2.0f, 0.0f);
+	vertices[6].texture = D3DXVECTOR2(0.0f, 1.0f);
+	vertices[7].position = D3DXVECTOR3(-5.0f, 2.0f, 0.0f);
+	vertices[7].texture = D3DXVECTOR2(0.0f, 0.0f);
+	vertices[8].position = D3DXVECTOR3(-1.0f, 2.0f, 0.0f);
+	vertices[8].texture = D3DXVECTOR2(1.0f, 0.0f);
+
+	vertices[9].position = D3DXVECTOR3(-5.0f, -2.0f, 0.0f);
+	vertices[9].texture = D3DXVECTOR2(0.0f, 1.0f);
+	vertices[10].position = D3DXVECTOR3(-1.0f, 2.0f, 0.0f);
+	vertices[10].texture = D3DXVECTOR2(1.0f, 0.0f);
+	vertices[11].position = D3DXVECTOR3(-1.0f, -2.0f, 0.0f);
+	vertices[11].texture = D3DXVECTOR2(1.0f, 1.0f);
+
+	// Load the index array with data.
+	indices[0] = 0; // Bottom left.
+	indices[1] = 1; // Top middle.
+	indices[2] = 2; // Bottom right.
+
+	indices[3] = 3;
+	indices[4] = 4;
+	indices[5] = 5;
+
+	indices[6] = 6;
+	indices[7] = 7;
+	indices[8] = 8;
+
+	indices[9] = 9;
+	indices[10] = 10;
+	indices[11] = 11;
+
+	// Set up the description of the static vertex buffer.
+	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = sizeof(VertexType) * m_vertexCount;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.MiscFlags = 0;
+	vertexBufferDesc.StructureByteStride = 0;
+	// Give the subresource structure a pointer to the vertex data.
+	vertexData.pSysMem = vertices;
+	vertexData.SysMemPitch = 0;
+	vertexData.SysMemSlicePitch = 0;
+	// Now create the vertex buffer.
+	result = device->CreateBuffer(&vertexBufferDesc, &vertexData, &m_vertexBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+	// Set up the description of the static index buffer.
+	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	indexBufferDesc.ByteWidth = sizeof(unsigned long) * m_indexCount;
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+	indexBufferDesc.StructureByteStride = 0;
+	// Give the subresource structure a pointer to the index data.
+	indexData.pSysMem = indices;
+	indexData.SysMemPitch = 0;
+	indexData.SysMemSlicePitch = 0;
+	// Create the index buffer.
+	result = device->CreateBuffer(&indexBufferDesc, &indexData, &m_indexBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+
+
+	// Release the arrays now that the vertex and index buffers have been created and loaded.
+	delete[] vertices;
+	vertices = 0;
+	delete[] indices;
+	indices = 0;
+	return true;
+}
+
+void ModelClass::ShutdownBuffers()
+{
+	// Release the index buffer.
+	if (m_indexBuffer)
+	{
+		m_indexBuffer->Release();
+		m_indexBuffer = 0;
+	}
+	// Release the vertex buffer.
+	if (m_vertexBuffer)
+	{
+		m_vertexBuffer->Release();
+		m_vertexBuffer = 0;
+	}
+	return;
+}
+
+void ModelClass::RenderBuffers(ID3D11DeviceContext* deviceContext)
+{
+	unsigned int stride;
+	unsigned int offset;
+	// Set vertex buffer stride and offset.
+	stride = sizeof(VertexType);
+	offset = 0;
+
+	// Set the vertex buffer to active in the input assembler so it can be rendered.
+	deviceContext->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
+	// Set the index buffer to active in the input assembler so it can be rendered.
+	deviceContext->IASetIndexBuffer(m_indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+	// Set the type of primitive that should be rendered from this vertex buffer, in this case triangles.
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	return;
+}
+
+bool ModelClass::LoadTexture(ID3D11Device* device, WCHAR* filename)
+{
+	bool result;
+	// Create the texture object.
+	m_Texture = new TextureClass;
+	if (!m_Texture)
+	{
+		return false;
+	}
+	// Initialize the texture object.
+	result = m_Texture->Initialize(device, filename);
+	if (!result)
+	{
+		return false;
+	}
+	return true;
+}
+
+void ModelClass::ReleaseTexture()
+{
+	// Release the texture object.
+	if (m_Texture)
+	{
+		m_Texture->Shutdown();
+		delete m_Texture;
+		m_Texture = 0;
+	}
+	return;
+}
